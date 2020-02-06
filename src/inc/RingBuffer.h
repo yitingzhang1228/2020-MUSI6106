269cacd163416dbd293a65b7a17644de6e653f92
yiting <yitingzhang1228@gmail.com>
2020-02-05 21:25:51 -0500

    update RingBuffer.h

--- src/inc/RingBuffer.h
+++ src/inc/RingBuffer.h
@@ -11,17 +11,25 @@ template <class T>
 class CRingBuffer
 {
 public:
+    // explicit: no inexplicit type conversion
     explicit CRingBuffer (int iBufferLengthInSamples) :
-        m_iBuffLength(iBufferLengthInSamples)
+        m_iBuffLength(iBufferLengthInSamples),
+        m_iReadIdx(0),
+        m_iWriteIdx(0)
     {
         assert(iBufferLengthInSamples > 0);
 
         // allocate and init
+        m_ptBuff = new T[iBufferLengthInSamples];
+        reset();
+        
     }
 
     virtual ~CRingBuffer ()
     {
         // free memory
+        delete [] m_ptBuff;
+        m_ptBuff = 0;
     }
 
     /*! add a new value of type T to write index and increment write index
@@ -30,6 +38,9 @@ public:
     */
     void putPostInc (T tNewValue)
     {
+        //write and increment index by one
+        put(tNewValue);
+        m_iWriteIdx = mod(m_iWriteIdx+1);
     }
 
     /*! add new values of type T to write index and increment write index
@@ -39,6 +50,7 @@ public:
     */
     void putPostInc (const T* ptNewBuff, int iLength)
     {
+        //not now
     }
 
     /*! add a new value of type T to write index
@@ -47,6 +59,8 @@ public:
     */
     void put(T tNewValue)
     {
+        //only write
+        m_ptBuff[m_iWriteIdx] = tNewValue;
     }
 
     /*! add new values of type T to write index
@@ -56,6 +70,7 @@ public:
     */
     void put(const T* ptNewBuff, int iLength)
     {
+        //not now
     }
     
     /*! return the value at the current read index and increment the read pointer
@@ -63,7 +78,9 @@ public:
     */
     T getPostInc ()
     {
-        return static_cast<T>(-1);
+        T temp = get();
+        m_iReadIdx = mod(m_iReadIdx+1);
+        return temp;
     }
 
     /*! return the values starting at the current read index and increment the read pointer
@@ -73,6 +90,7 @@ public:
     */
     void getPostInc (T* ptBuff, int iLength)
     {
+        // not now
     }
 
     /*! return the value at the current read index
@@ -81,7 +99,12 @@ public:
     */
     T get (float fOffset = 0.f) const
     {
-        return static_cast<T>(-1);
+        // not only read current index
+        // can read current index +/- something
+        // fractional delay line
+        // linear interpolation between two samples
+        // for this exercise: interger offset only
+        return m_ptBuff[mod(m_iReadIdx + iOffset)];
     }
 
     /*! return the values starting at the current read index
@@ -91,6 +114,7 @@ public:
     */
     void get (T* ptBuff, int iLength) const
     {
+        // not now
     }
     
     /*! set buffer content and indices to 0
@@ -98,6 +122,9 @@ public:
     */
     void reset ()
     {
+        m_iReadIdx  = 0;
+        m_iWriteIdx = 0;
+        memset(m_ptBuff, 0, sizeof(T) * m_iBuffLength);
     }
 
     /*! return the current index for writing/put
@@ -105,7 +132,7 @@ public:
     */
     int getWriteIdx () const
     {
-        return -1;
+        return m_iReadIdx;
     }
 
     /*! move the write index to a new position
@@ -114,6 +141,7 @@ public:
     */
     void setWriteIdx (int iNewWriteIdx)
     {
+        m_iWriteIdx = mod(iNewWriteIdx);
     }
 
     /*! return the current index for reading/get
@@ -121,7 +149,7 @@ public:
     */
     int getReadIdx () const
     {
-        return -1;
+        return m_iReadIdx;
     }
 
     /*! move the read index to a new position
@@ -130,14 +158,17 @@ public:
     */
     void setReadIdx (int iNewReadIdx)
     {
+         m_iReadIdx = mod(iNewReadIdx);
     }
 
     /*! returns the number of values currently buffered (note: 0 could also mean the buffer is full!)
     \return int
     */
+    // const: this method can not change the internal state
+    // can not write
     int getNumValuesInBuffer () const
     {
-        return -1;
+        return (m_iWriteIdx - m_iReadIdx + m_iBuffLength) % m_iBuffLength;
     }
 
     /*! returns the length of the internal buffer
@@ -145,12 +176,27 @@ public:
     */
     int getLength () const
     {
-        return -1;
+        return m_iBuffLength;
     }
 private:
-    CRingBuffer ();
-    CRingBuffer(const CRingBuffer& that);
+    CRingBuffer ();  // another defualt constructor
+    // can't call the defualt constructor without mention how long the buffer will be
+    CRingBuffer(const CRingBuffer& that);  // copy constructor (private)
+    // copy all the members including pointers, the new class points to the buffer from previous instance, we want to avoid it
+    // two ways: lazy way (copy constructor private); complete way (write a copy constructor)
 
     int m_iBuffLength;              //!< length of the internal buffer
+    int m_iReadIdx;
+    int m_iWriteIdx;
+    T *m_ptBuff;
+    
+    int mod (int index)
+    {
+        while(index < 0){
+            index += m_iBuffLength;f
+        }
+        int remainder = index % m_iBuffLength;
+        return remainder;
+    }
 };
 #endif // __RingBuffer_hdr__
